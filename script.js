258b136b69ab5a25b26464fcd24b91b038fa3f4b
Tony <un_razum@mail.ru>
2021-08-22 16:43:01 +0300

    Update

---
+++ script.js
@@ -0,0 +1,13 @@
+const changeBtn = document.querySelector("button");
+changeBtn.addEventListener("click", changeColor);
+function changeColor() {
+  const hex_numbers = "0123456789ABCDEF";
+
+  let hexcode = "";
+  for (let i = 0; i < 6; i++) {
+    const random_index = Math.floor(Math.random() * hex_numbers.length);
+    hexcode += hex_numbers[random_index];
+  }
+  document.querySelector("#hex-code").textContent = hexcode;
+  document.body.style.background = "#"+hexcode;
+}
